228a27fe812a55a00d36473281bef3387576be09
nacho522 <ignaciocosta1234@gmail.com>
2022-03-15 22:26:51 -0300

    port

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const path = require ('path');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 const publicPath = path.resolve(__dirname, '../public');
